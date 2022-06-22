36fe92ec076c4c40b3ee68676671d3b62ace0d90
NathalyStudio <andreina3nathaly5@gmail.com>
2022-06-21 23:04:45 -0400

    fixed audio file

--- modules/js/audio.js
+++ modules/js/audio.js
@@ -1,57 +1,59 @@
-export default function videoPlayer() {
-    // llamamos a los elementos html
+function audioPlayer() {
 
-    const video = document.getElementById("video")
-    const videoContainer = document.getElementById("video-container")
-    const play = document.getElementById("play")
-    const control = document.getElementById("control")
-    const tenSeconds = document.getElementById("tenSeconds")
-    const thirtySeconds = document.getElementById("thirtySeconds")
-    const fullScreen = document.getElementById("fullScreen")
-    let duration;
-    let onFullScreen = false;
-    // Remmovemos los controles por defecto
-    video.removeAttribute("controls")
-        // creo un evento para tomar la duración
-    video.addEventListener("loadeddata", (e) => {
-            duration = e.target.duration
-        })
-        // Evento para tomar el porcentage de video que pasó. 
-    video.addEventListener("timeupdate", (e) => {
-            const percentage = (e.target.currentTime / duration) * 100 // sacamos la proporcion de tiempo que paso entre la cantidad de tiempo que paso y la duración total del video. Despues lo multiplico por 100 para sacar el porcentage.
-            control.value = percentage; // La barra de progreso avanza de acuerdo al porcentage de abance del video
-        })
-        // Controla el moviento de la barra de progreso para que la podamos posicionar donde queramos
-    control.oninput = (e) => {
-            video.currentTime = (duration / 100) * e.target.value; // divido la duracion entre 10 para obtener el porcentaje
-            // multiplico el porcentaje por e.target.value para que me de el porcentaje donde se debe colocar el
-        }
-        // función que adelanta el video 30 segundos
-    thirtySeconds.onclick = () => {
-            video.currentTime = video.currentTime + 30
-        }
-        // Función que retrocede el video 10 segundos
-    tenSeconds.onclick = () => {
-            video.currentTime = video.currentTime - 10
-        }
-        // Función para hacer la pantalla completa
-    fullScreen.onclick = () => {
-            if (onFullScreen) {
-                onFullScreen = false;
-                document.exitFullscreen()
-            } else {
-                onFullScreen = true;
-                videoContainer.requestFullscreen()
-            }
+    const audio = document.getElementById("audio")
+    const play = document.getElementById("playAudio")
+    const control = document.getElementById("controlAudio")
+    const tenSeconds = document.getElementById("tenSecondsAudio")
+    const thirtySeconds = document.getElementById("thirtySecondsAudio")
+    let audioDuration
 
-        }
-        // función para dar play and stop a nuestro video
-    play.onclick = () => {
-        if (video.paused) {
-            video.play()
+
+
+    // creo un evento para tomar la duración del audio
+
+      audio.addEventListener("loadeddata", (e) =>{
+        audioDuration = e.target.duration
+    })
+
+
+    // función para pausar y adelantar el audio
+
+    play.onclick = () => {         
+        if(audio.paused) {
+            audio.play()
         } else {
-            video.pause()
+            audio.pause()
         }
     }
 
+    // función para retroceder 10 segundos
+
+    tenSeconds.onclick = () => {                             
+        audio.currentTime = audio.currentTime-10
+    }
+
+    // función para avanzar 30 segundos 
+
+    thirtySeconds.onclick = () => {
+        audio.currentTime = audio.currentTime+30   
+    }
+    
+
+    //ESTO DA ERROR PREGUNTAR
+
+    // Controla el moviento de la barra de progreso para que la podamos posicionar donde queramos
+    control.oninput = (e) => {
+        audio.currentTime = (audioDuration/100)*e.target.value; // divido la duracion entre 10 para obtener el porcentaje
+         // multiplico el porcentaje por e.target.value para que me de el porcentaje donde se debe colocar el
+    }
+    
+
+
+
+
+    
+
+
 }
+
+export default audioPlayer
